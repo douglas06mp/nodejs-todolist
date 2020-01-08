3b1e080c1f68b72aa410dbea0897caf4d10889e0
douglas <douglas06mp@gmail.com>
2020-01-08 15:59:30 +0800

    prepare to deploy on heroku

--- app.js
+++ app.js
@@ -118,7 +118,10 @@ app.get("/:listName", function(req, res){
 });
 
 
-
-app.listen(3000, function(){
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, function(){
   console.log("Server started");
 });
